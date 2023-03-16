ae083217c256b75c148aa74dd16e51c1c69cd9db
Endryll <endryll.silverio.dantas@gmail.com>
2023-03-16 16:22:00 -0300

    Criação do codigo python para o calculo de idade

---
+++ Projeto_python.py
@@ -0,0 +1,9 @@
+nascimento = 2005
+anoAtual = 2023
+
+def calculoIdade(nascimento, anoAtual):
+    idade=anoAtual - nascimento
+    return idade
+
+idade=calculoIdade(nascimento, anoAtual)
+print(idade)
