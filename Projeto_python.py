190afc33e375d74f97ccd492cecdd9ce4edb7e3d
Endryll <88735167+endryll@users.noreply.github.com>
2023-03-16 16:39:22 -0300

    Delete Projeto_python.py

--- Projeto_python.py
+++
@@ -1,9 +0,0 @@
-nascimento = 2005
-anoAtual = 2023
-
-def calculoIdade(nascimento, anoAtual):
-    idade=anoAtual - nascimento
-    return idade
-
-idade=calculoIdade(nascimento, anoAtual)
-print(idade)
